df749405ec2e1ed1cf263d62679ef4df66c9ee19
Matthew Anderson <anderson.matt1397@gmail.com>
2020-01-23 17:33:29 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -13,6 +13,7 @@ elsif phrase == phrase.upcase
 return "NO, NOT SINCE 1938!"
 # NO, NOT SINCE 1938!
 else  
+  return "HUH?! SPEAK UP, SONNY!"
 # However if you say 'I LOVE YOU GRANDMA!', she should respond with
 # 'I LOVE YOU TOO PUMPKIN!'
 end
