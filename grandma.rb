9946fd5790d7e56b42327018fb61ba1aebf787f8
Matthew Anderson <anderson.matt1397@gmail.com>
2020-01-23 16:32:11 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -3,7 +3,7 @@ def speak_to_grandma(phrase)
 # Whatever you say to grandma, she should respond with
 # HUH?! SPEAK UP, SONNY!
 # unless you shout it (type in all capitals).
-phrase.downcase == phrase.upcase
+phrase == phrase.upcase
 if phrase
   puts "HUH? SPEAK UP, SONNY!"
 # If you shout, she can hear you (or at least she thinks so) 
