e811ef24fe01342a97b342c7f1d6a94e3591f731
Matthew Anderson <anderson.matt1397@gmail.com>
2020-01-23 17:31:13 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -7,7 +7,6 @@ def speak_to_grandma(phrase)
   
  if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-   "NO, NOT SINCE 1938!"
 # If you shout, she can hear you (or at least she thinks so) 
 # and yells back
 elsif phrase.
